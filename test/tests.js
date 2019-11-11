466d5ead574683c4918ececbe40c7cc41dea8e46
Mike Chu <itsmikechu@users.noreply.github.com>
2019-11-11 05:55:15 +0100

    Fix bugs in test

--- test/tests.js
+++ test/tests.js
@@ -3,8 +3,8 @@ const e2rpg = require('../lib')
 
 describe('Main functionality', () => {
   it('displays a friendly welcome message', () => {
-    const message = e2rpg()
+    const message = e2rpg.message
 
-    expect(mesesage).to.equal('Hello E2RPG. :-)')
+    expect(message).to.equal('Hello E2RPG. :-)')
   })
 })
