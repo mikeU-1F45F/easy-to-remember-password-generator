ad3de11502c731a9c6e8e96c18191afc9afcb885
Mike Chu <itsmikechu@users.noreply.github.com>
2019-11-20 05:09:43 +0100

    Update spec for random integer

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -1,13 +1,17 @@
 import { Utils } from './utils';
 
 describe('Utils', () => {
-  it('produces a random positive integer >= 0', () => {
+  it('produces a random integer between a minimum and maximum', () => {
+    const smallestPossibleInteger: number = 0;
     const largestPossibleInteger: number = 17;
-    const selectedRandomInteger: number = Utils.random0OrGreaterInteger(
+    const selectedRandomInteger: number = Utils.randomInteger(
+      smallestPossibleInteger,
       largestPossibleInteger,
     );
 
-    expect(selectedRandomInteger).toBeGreaterThanOrEqual(0);
+    expect(selectedRandomInteger).toBeGreaterThanOrEqual(
+      smallestPossibleInteger,
+    );
     expect(selectedRandomInteger).toBeLessThanOrEqual(largestPossibleInteger);
   });
 });
