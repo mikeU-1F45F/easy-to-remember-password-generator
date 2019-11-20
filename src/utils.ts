fd501b28480b90c8792b7f9c4a8fd7d3a2b09bfc
Mike Chu <itsmikechu@users.noreply.github.com>
2019-11-20 05:57:53 +0100

    Satisfy utils spec

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,19 @@
 export class Utils {
+  public static titleCase(wordToTitleCase: string): string {
+    if (wordToTitleCase.indexOf(' ') >= 0) {
+      throw new Error('Function operates on single word');
+    }
+    return (
+      wordToTitleCase.charAt(0).toUpperCase() +
+      wordToTitleCase.substr(1).toLowerCase()
+    );
+  }
+
   public static randomInteger(
     smallestPossibleInteger: number,
     largestPossibleInteger: number,
   ): number {
-    const min: number = 0;
+    const min: number = Math.floor(smallestPossibleInteger);
     const max: number = Math.floor(largestPossibleInteger);
 
     return Math.floor(Math.random() * (max - min)) + min; // The maximum is exclusive and the minimum is inclusive
