0ecd79d5b5f4975850221cddb6f0fce3004b80c3
Mike Chu <itsmikechu@users.noreply.github.com>
2019-11-20 05:33:48 +0100

    Add negative case for word util

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -1,23 +1,31 @@
 import { Utils } from './utils';
 
 describe('Utils', () => {
-  it('produces a random integer between a minimum and maximum', () => {
-    const smallestPossibleInteger: number = 0;
-    const largestPossibleInteger: number = 17;
-    const selectedRandomInteger: number = Utils.randomInteger(
-      smallestPossibleInteger,
-      largestPossibleInteger,
-    );
+  describe('for numbers', () => {
+    it('produces a random integer between a minimum and maximum', () => {
+      const smallestPossibleInteger: number = 0;
+      const largestPossibleInteger: number = 17;
+      const selectedRandomInteger: number = Utils.randomInteger(
+        smallestPossibleInteger,
+        largestPossibleInteger,
+      );
 
-    expect(selectedRandomInteger).toBeGreaterThanOrEqual(
-      smallestPossibleInteger,
-    );
-    expect(selectedRandomInteger).toBeLessThanOrEqual(largestPossibleInteger);
+      expect(selectedRandomInteger).toBeGreaterThanOrEqual(
+        smallestPossibleInteger,
+      );
+      expect(selectedRandomInteger).toBeLessThanOrEqual(largestPossibleInteger);
+    });
   });
 
-  it('produces a title-case version of a word', () => {
-    const titleCaseVersion: string = Utils.titleCase('bananas');
+  describe('for words', () => {
+    it('produces a title-case version of a word', () => {
+      const titleCaseVersion: string = Utils.titleCase('bananas');
 
-    expect(titleCaseVersion).toEqual('Bananas');
+      expect(titleCaseVersion).toEqual('Bananas');
+    });
+
+    it('throws an error when you try to provide multiple words', () => {
+      expect(Utils.titleCase('happy shoes')).toThrowError();
+    });
   });
 });
